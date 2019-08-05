f14cc026eca5c4124d2c69c20f2f75ee47944ad6
Serhii CHORNONOH <schornon@e1r9p15.unit.ua>
2019-08-05 13:58:10 +0300

    Was created project

---
+++ NewsWeatherProfile/NewsWeatherProfile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsWeatherProfile
+//
+//  Created by Serhii CHORNONOH on 8/5/19.
+//  Copyright © 2019 Serhii CHORNONOH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
