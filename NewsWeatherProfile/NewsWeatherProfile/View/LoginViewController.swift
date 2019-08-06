0599ea0721a164a42ba86a9fddf805ead87639db
Serhii CHORNONOH <schornon@e1r9p15.unit.ua>
2019-08-06 12:03:45 +0300

    pod install Firebase/Auth

--- NewsWeatherProfile/NewsWeatherProfile/View/LoginViewController.swift
+++ NewsWeatherProfile/NewsWeatherProfile/View/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  NewsWeatherProfile
 //
 //  Created by Serhii CHORNONOH on 8/5/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ NewsWeatherProfile/NewsWeatherProfile/ViewModel/LoginViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewModel.swift
+//  NewsWeatherProfile
+//
+//  Created by Serhii CHORNONOH on 8/5/19.
+//  Copyright © 2019 Serhii CHORNONOH. All rights reserved.
+//
+
+import Foundation
+
+class LoginViewModel {
+    
+}
